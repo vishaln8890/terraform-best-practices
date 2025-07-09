5190b51ecd0a6562743a741b8462cb0444c5845c
vishaln8890 <vishaln8890@gmail.com>
2025-07-09 13:06:21 +0000

    Use the self variable

--- main.tf
+++ main.tf
@@ -5,3 +5,17 @@ resource "aws_instance" "instance_1" {
     Name = var.instance_1_name
   }
 }
+
+resource "aws_instance" "instance_2" {
+  ami           = var.instance_2_ami
+  instance_type = var.instance_2_type
+  tags = {
+    Name = var.instance_2_name
+  }
+
+  provisioner "local-exec" {
+    command    = "echo The IP address of the Server is ${self.private_ip}"
+    on_failure = continue
+  }
+}
+
--- variables.tf
+++ variables.tf
@@ -15,3 +15,21 @@ variable "instance_1_name" {
   type        = string
   default     = "instance-1"
 }
+
+variable "instance_2_ami" {
+  description = "AMI ID of EC2 instance"
+  type        = string
+  default     = "ami-0f918f7e67a3323f0"
+}
+
+variable "instance_2_type" {
+  description = "Type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_2_name" {
+  description = "Name of EC2 instance"
+  type        = string
+  default     = "instance-2"
+}
